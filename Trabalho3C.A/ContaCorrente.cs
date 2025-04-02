673929de001c964800af42a5b2246589e2bb7e79
Iagodizlla <iagoschlemper@gmail.com>
2025-04-02 18:22:23 -0300

    Adicionar arquivos de projeto.

---
+++ Trabalho3C.A/ContaCorrente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho3C.A
+{
+    internal class ContaCorrente
+    {
+        public int numero { get; set; }
+        public double saldo { get; set; }
+        public double limite { get; set; }
+        }
+    }
+}
---
+++ Trabalho3C.A/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Trabalho3C.A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta1 = new ContaCorrente();
+
+            conta1.saldo = 1000;
+
+            conta1.numero = 12;
+
+            conta1.limite = 0;
+
+            conta1.movimentacoes = new Movimentacao[10];
+
+            conta1.Sacar(200);
+
+            conta1.Depositar(300);
+
+            conta1.Depositar(500);
+
+            conta1.Sacar(200);
+
+            ContaCorrente conta2 = new ContaCorrente();
+
+            conta2.saldo = 300;
+
+            conta2.numero = 13;
+
+            conta2.limite = 0;
+
+            conta2.movimentacoes = new Movimentacao[10];
+
+            conta1.TransferirPara(400);
+
+            conta1.ExibirExtrato);
+
+            conta2.ExibirExtrato();
+        }
+    }
+}
